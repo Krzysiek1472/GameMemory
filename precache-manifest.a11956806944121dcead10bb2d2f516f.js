75cd0553cda3aa5e804b1f22554db619357a2488
Krzysiek1472 <krzysiek1472@gmail.com>
2019-08-10 20:38:47 +0200

    Updates

---
+++ precache-manifest.a11956806944121dcead10bb2d2f516f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4e199246c31eb1e5294d7948105dd6",
+    "url": "/GameMemory/index.html"
+  },
+  {
+    "revision": "e45d4f5d9d4712f44a2c",
+    "url": "/GameMemory/static/css/main.9a52b317.chunk.css"
+  },
+  {
+    "revision": "37b5bf4c10069c5c7c69",
+    "url": "/GameMemory/static/js/2.d286d434.chunk.js"
+  },
+  {
+    "revision": "e45d4f5d9d4712f44a2c",
+    "url": "/GameMemory/static/js/main.a2e40988.chunk.js"
+  },
+  {
+    "revision": "f7b608a861c9eea08252",
+    "url": "/GameMemory/static/js/runtime~main.c2626e14.js"
+  },
+  {
+    "revision": "3d45b3b3ddbce73bfe90ecce27ecfb2d",
+    "url": "/GameMemory/static/media/unseen.3d45b3b3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0bdf1942b2becc411ed44825a8c0ee2.js"
+  "/GameMemory/precache-manifest.a11956806944121dcead10bb2d2f516f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GameMemory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
